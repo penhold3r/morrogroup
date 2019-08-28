2026252e03c22466b4b2f20c38e0a9c648aa7f00
penholder <penhold3r@gmail.com>
2019-08-28 14:10:38 -0300

    contact tweaks

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,12 +11,12 @@ const Contact = () => {
 		>
 			<div className="contact__content">
 				<div className="contact-data px-1">
-					<h2 className="contact-title text-color-white">Contacto</h2>
-					<a href="tel:132465789" className="phone icon icon-phone text-color-white">
-						132 <strong>465 789</strong>
-					</a>
-					<a href="tel:132465789" className="phone icon icon-phone text-color-white">
-						132 <strong>465 789</strong>
+					<h2 className="contact-title text-color-white m-none mb-1">Contacto</h2>
+					<a
+						href="tel:132465789"
+						className="phone icon icon-phone text-color-white m-none mb-1"
+					>
+						263 <strong>453 7898</strong>
 					</a>
 					<a
 						href="mailto:hola@morrogroup.com"
@@ -25,41 +25,56 @@ const Contact = () => {
 						<strong>hola</strong>@morrogroup.com
 					</a>
 
-					<ul className="social">
+					<ul className="social my-2 p-none">
 						<li className="social__icon">
 							<a href="http://facebook.com" target="_blank" rel="noopener noreferrer">
-								<i className="icon icon-facebook" />
+								<i className="icon icon-facebook text-color-white" />
 							</a>
 						</li>
 						<li className="social__icon">
 							<a href="http://instagram.com" target="_blank" rel="noopener noreferrer">
-								<i className="icon icon-instagram" />
+								<i className="icon icon-instagram text-color-white" />
 							</a>
 						</li>
 						<li className="social__icon">
 							<a href="http://twitter.com" target="_blank" rel="noopener noreferrer">
-								<i className="icon icon-twitter" />
+								<i className="icon icon-twitter text-color-white" />
 							</a>
 						</li>
 						<li className="social__icon">
 							<a href="http://whatsapp.com" target="_blank" rel="noopener noreferrer">
-								<i className="icon icon-whatsapp" />
+								<i className="icon icon-whatsapp text-color-white" />
 							</a>
 						</li>
 					</ul>
 				</div>
 				<form className="contact-form px-1">
 					<div className="form-block mb-1">
-						<input className="form-input" type="text" placeholder="Nombre" />
+						<input
+							className="form-input px-1 py-h-1 text-color-black"
+							type="text"
+							placeholder="Nombre"
+						/>
 					</div>
 					<div className="form-block mb-1">
-						<input className="form-input" type="email" placeholder="Email" />
+						<input
+							className="form-input px-1 py-h-1 text-color-black"
+							type="email"
+							placeholder="Email"
+						/>
 					</div>
 					<div className="form-block mb-1">
-						<textarea className="form-input textarea" placeholder="Mensaje" />
+						<textarea
+							className="form-input px-1 py-h-1 text-color-black textarea"
+							placeholder="Mensaje"
+						/>
 					</div>
 
-					<input className="form-submit" type="submit" value="Enviar" />
+					<input
+						className="form-submit px-1 py-h-1 text-color-white"
+						type="submit"
+						value="Enviar"
+					/>
 				</form>
 			</div>
 		</section>
